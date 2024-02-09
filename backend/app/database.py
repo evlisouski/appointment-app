12c3b8df5cac944166f6881cb48023523f9db01e
Eduard Lisouski <evlisouski@gmail.com>
2024-02-10 01:01:41 +0300

    feat: prepared the environment for adding test data, added test data for tables customers, providers, tags, providers_tags

--- backend/app/database.py
+++ backend/app/database.py
@@ -5,14 +5,16 @@ from sqlalchemy.ext.asyncio import async_sessionmaker
 
 from app.config import settings
 
-DATABASE_URL = settings.DATABASE_URL
-DATABASE_PARAMS = {}
+if settings.MODE == "TEST":
+    DATABASE_URL = settings.TEST_DATABASE_URL
+    DATABASE_PARAMS = {"poolclass": NullPool}
+else:
+    DATABASE_URL = settings.DATABASE_URL
+    DATABASE_PARAMS = {}
 
 engine = create_async_engine(DATABASE_URL, **DATABASE_PARAMS)
 
-
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
-
 
 class Base(DeclarativeBase):
     pass
---
+++ backend/app/tests/conftest.py
@@ -0,0 +1,57 @@
+import asyncio
+from datetime import datetime
+import json
+import pytest
+from sqlalchemy import insert
+from app.config import settings
+from app.database import Base, async_session_maker, engine
+
+from app.customers.models import Customer
+from app.providers.models import Provider, ProviderTag, Tag
+
+@pytest.fixture(scope="session", autouse=True)
+async def prepare_database():
+    assert settings.MODE == "TEST"
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+    def open_mock_json(model: str):
+        with open(f"app/tests/mock_{model}.json", "r") as file:
+            return json.load(file)
+
+    customers = open_mock_json("customers")
+    providers = open_mock_json("providers")
+    tags = open_mock_json("tags")
+    providers_tags = open_mock_json("providers_tags")
+
+    for customer in customers:
+        customer["birthday"] = datetime.strptime(customer["birthday"], "%Y-%m-%d")
+        customer["registration_date"] = datetime.strptime(customer["registration_date"], "%Y-%m-%d")
+
+    for provider in providers:
+        provider["foundation_date"] = datetime.strptime(provider["foundation_date"], "%Y-%m-%d")
+        provider["registration_date"] = datetime.strptime(provider["registration_date"], "%Y-%m-%d")
+
+    async with async_session_maker() as session:
+        add_customers = insert(Customer).values(customers)
+        add_providers = insert(Provider).values(providers)
+        add_tags = insert(Tag).values(tags)
+        add_providers_tags = insert(ProviderTag).values(providers_tags)
+
+        await session.execute(add_customers)
+        await session.execute(add_providers)
+        await session.execute(add_tags)
+        await session.execute(add_providers_tags)
+
+        await session.commit()
+
+# Взято из документации к pytest-asyncio
+# Создаем новый event loop для прогона тестов
+@pytest.fixture(scope="session")
+def event_loop(request):
+    """Create an instance of the default event loop for each test case."""
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
