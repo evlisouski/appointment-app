9d9b02b8e77f28b4731d9327e5944b1fc63252ec
Eduard <evlisouski@gmail.com>
2024-03-07 00:08:56 +0300

    save: maintain a workable project

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ import time
 from uu import decode
 from black import Encoding
 from fastapi import FastAPI, Request
+from fastapi_versioning import VersionedFastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 import sentry_sdk
 from sqladmin import Admin
 
@@ -16,6 +18,7 @@ from app.users.models import User
 from app.database import engine
 from app.admin.views import CustomerAdmin, UserAdmin, ProviderAdmin, TagAdmin, AppointmentAdmin
 from app.admin.auth import authentication_backend
+from app.prometheus.router import router as router_prometeus
 
 
 from fastapi_cache import FastAPICache
@@ -33,11 +36,18 @@ sentry_sdk.init(
 
 app = FastAPI()
 
+
 app.include_router(router_auth)
 app.include_router(router_users)
 app.include_router(router_providers)
 app.include_router(router_customers)
 app.include_router(router_appointments)
+
+# Подключение версионирования
+# app = VersionedFastAPI(app,
+#                        version_format='{major}',
+#                        prefix_format='/api/v{major}',
+#                        )
 
 admin = Admin(app, engine, authentication_backend=authentication_backend)
 
@@ -46,11 +56,21 @@ admin.add_view(CustomerAdmin)
 admin.add_view(ProviderAdmin)
 admin.add_view(TagAdmin)
 admin.add_view(AppointmentAdmin)
+app.include_router(router_prometeus)
+
+instrumentator = Instrumentator(
+    should_group_status_codes=False,
+    excluded_handlers=[".*admin.*", "/metrics"],
+    )
+instrumentator.instrument(app).expose(app)
 
 @app.on_event("startup")
-async def startup():
+async def startup():    
+  
     redis = aioredis.from_url("redis://localhost:6379", encoding="utf8", decode_responses=True)
     FastAPICache.init(RedisBackend(redis), prefix="cache")
+
+
 
 
 @app.middleware("http")
@@ -63,3 +83,5 @@ async def add_process_time_header(request: Request, call_next):
         "process_time": round(process_time, 4)
     })
     return response
+
+
